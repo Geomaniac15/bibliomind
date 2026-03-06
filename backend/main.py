ce2cef6593f97464c45232f5aae67e31ded113ff
gveal <gveal07@outlook.com>
2026-03-06 01:42:09 +0000

    trying to implement database search for books

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,43 @@ import pytesseract
 import cv2
 import numpy as np
 import os 
+import requests 
 
 os.environ['TESSDATA_PREFIX'] = r"C:\Program Files\Tesseract-OCR\tessdata"
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+def clean_ocr_text(text):
+    lines = text.split('\n')
+
+    # remove empty lines
+    lines = [line.strip() for line in lines if line.strip() != '']
+
+    # remove numbers like "1"
+    lines = [line for line in lines if not line.isdigit()]
+
+    # join into one search string
+    cleaned = ' '.join(lines)
+
+    return cleaned
+
+def search_openlibrary(query):
+    url = 'https://openlibrary.org/search.json'
+    params = {'q': query}
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if data['numFound'] > 0:
+        book = data['docs'][0]
+
+        return {
+            'title': book.get('title'),
+            'author': book.get('author_name', ['Unknown'])[0],
+            'year': book.get('first_publish_year'),
+        }
+    
+    return None
 
 app = FastAPI()
 
@@ -18,6 +51,15 @@ async def scan_books(file: UploadFile):
         np.frombuffer(contents, np.uint8),
         cv2.IMREAD_COLOR
     )
-    text = pytesseract.image_to_string(image)
 
-    return {'detected_text': text}
+    text = pytesseract.image_to_string(image, lang='eng')
+
+    cleaned_text = clean_ocr_text(text)
+
+    book = search_openlibrary(cleaned_text)
+
+    return {
+        'ocr_text': text,
+        'cleaned_text': cleaned_text,
+        'matched_book': book
+    }
